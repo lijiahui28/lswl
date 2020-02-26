080df3c7546a9c7a794372dc98f5e9b973b3f22f
lijiahui28 <2653380778@qq.com>
2020-02-26 22:16:42 +0800

    第二次提交

--- app/src/component/Manage/Manage .jsx
+++ app/src/component/Manage/Manage .jsx
@@ -1,11 +1,18 @@
 import React,{Component} from 'react'
+import './Manage.css'
 export default class Manage extends Component{
 constructor(){
     super()
 }
 render(){
     return(
-        <div>理账</div>
+        <div>
+            <div style={{width:"200px",margin:"0 auto",marginTop:"20px",overflow:"hidden",borderRadius:"5px",border:"1px solid #199ed8"}}>
+                <div style={{width:"50%",float:"left",backgroundColor:"#199ed8",textAlign:"center",color:"#fff",fontSize:"16px",height:"40px",lineHeight:"40px"}}>收礼</div>
+                <div style={{width:"50%",float:"left",textAlign:"center",color:"#199ed8",fontSize:"16px",height:"40px",lineHeight:"40px"}}>送礼</div>
+            </div>
+        </div>
+        
     )
 }
 }
