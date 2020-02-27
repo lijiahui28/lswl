772b78241492c055740162bac7e7185830db2cc6
lijiahui28 <2653380778@qq.com>
2020-02-27 22:33:08 +0800

    布局

--- app/src/component/Manage/Manage .jsx
+++ app/src/component/Manage/Manage .jsx
@@ -1,15 +1,37 @@
 import React,{Component} from 'react'
 import './Manage.css'
 export default class Manage extends Component{
-constructor(){
-    super()
-}
 render(){
     return(
         <div>
-            <div style={{width:"200px",margin:"0 auto",marginTop:"20px",overflow:"hidden",borderRadius:"5px",border:"1px solid #199ed8"}}>
-                <div style={{width:"50%",float:"left",backgroundColor:"#199ed8",textAlign:"center",color:"#fff",fontSize:"16px",height:"40px",lineHeight:"40px"}}>收礼</div>
-                <div style={{width:"50%",float:"left",textAlign:"center",color:"#199ed8",fontSize:"16px",height:"40px",lineHeight:"40px"}}>送礼</div>
+            <div style={{backgroundColor:"#fff",padding:"10px 0"}}>
+                <div className="manage-top">
+                    <div className="manage-top-l">收礼</div>
+                    <div className="manage-top-r">送礼</div>
+                </div>
+            </div>
+            
+            <div className="manage-sel">
+                <div className="sel-item">类别<img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u228.png?token=8f6e29af0909d6517b5f170d8ebe78e664c99274a27307872d88ce3f8688c67f" alt=""/></div>
+                <div className="sel-item">时间<img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u228.png?token=8f6e29af0909d6517b5f170d8ebe78e664c99274a27307872d88ce3f8688c67f" alt=""/></div>
+                <div className="sel-item">金额<img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u228.png?token=8f6e29af0909d6517b5f170d8ebe78e664c99274a27307872d88ce3f8688c67f" alt=""/></div>
+            </div>
+            <div className="manage-item">
+                <div className="manage-item-top">
+                    <div className="manage-item-t-l">
+                        <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u102.png?token=4e77da7d4f51db07c47f79cb5ad8fcb1979399ae51a0122ba2e1ed19ec5e09ec" alt=""/>项目类型
+                    </div>
+                    <div className="manage-item-t-r">15000.00</div>
+                </div>
+                <div className="manage-item-bottom">
+                    <div className="manage-item-t-l">
+                        <span>收</span>姓名
+                    </div>
+                    <div className="manage-item-t-r2">
+                        <span>10人送礼</span>
+                        <span>2020.02.02</span>
+                    </div>
+                </div>
             </div>
         </div>
         
--- app/src/component/director/director.jsx
+++ app/src/component/director/director.jsx
@@ -1,11 +1,39 @@
 import React,{Component} from 'react'
+import './director.css'
 export default class director extends Component{
 constructor(){
     super()
 }
 render(){
     return(
-        <div>礼事</div>
+        <div>
+            <div style={{backgroundColor:"#fff",padding:"10px 0"}}>
+                <div className="director-top">
+                    <div className="director-top-l">待参加</div>
+                    <div className="director-top-r">已参加</div>
+                </div>
+            </div>
+            <div className="director-sel">
+                <div className="sel-item">类别<img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u228.png?token=8f6e29af0909d6517b5f170d8ebe78e664c99274a27307872d88ce3f8688c67f" alt=""/></div>
+                <div className="sel-item">时间<img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u228.png?token=8f6e29af0909d6517b5f170d8ebe78e664c99274a27307872d88ce3f8688c67f" alt=""/></div>
+            </div>
+            <div className="director-item">
+                <div className="director-item-top">
+                    <div className="director-item-t-l">
+                        搬家请客
+                    </div>
+                    <div className="director-item-t-r">待参加</div>
+                </div>
+                <div className="director-item-bottom">
+                    <div className="director-item-t-l">
+                        <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼事列表/u478.svg?token=dd67287e4f4be0db781b30a6ac13ac422054d4a62d5bf46395b52bbc16d066ed" alt=""/>地点名称
+                    </div>
+                    <div className="director-item-t-r2">
+                        <span>2020.02.02</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 }
