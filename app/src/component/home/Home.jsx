29f3c8a0db6780006e352555544fb5b47151a984
lijiahui28 <2653380778@qq.com>
2020-03-02 09:22:43 +0800

    布局完成

--- app/src/component/home/Home.jsx
+++ app/src/component/home/Home.jsx
@@ -22,7 +22,41 @@ export default class Home extends Component{
 							<p>2500.00</p>
 						</div>
 					</div>
-					<div id="home-top2"></div>
+					<div id="home-top2">
+						<div>
+							<p>6月收礼</p>
+							<p>5500.00</p>
+						</div>
+						<div style={{borderRadius: '50%',backgroundColor: 'blue',}}>
+							<p style={{color:'#fff'}}>本月盈亏</p>
+							<p style={{color:'#fff'}}>2948.00</p>
+						</div>
+						<div>
+							<p>六月送礼</p>
+							<p>2500.00</p>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div className="home-tag">
+						<div className="home-tag-item">排行</div>
+						<div className="home-tag-item">用户</div>
+						<div className="home-tag-item">历史盈亏</div>
+						<div className="home-tag-item">本月盈亏</div>
+						<div className="home-tag-item">更多</div>
+					</div>
+					<div className="home-content">
+						<div className="home-content-item">1</div>
+						<div className="home-content-item">aaaaaa</div>
+						<div className="home-content-item">2500.00</div>
+						<div className="home-content-item">25000.00</div>
+					</div>
+					<div className="home-content">
+						<div className="home-content-item">2</div>
+						<div className="home-content-item">bbbbb</div>
+						<div className="home-content-item">2500.00</div>
+						<div className="home-content-item">25000.00</div>
+					</div>
 				</div>
 			</div>
 		)
